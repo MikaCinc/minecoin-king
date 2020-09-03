202a03b42aa30dfc6264d135bf3a9db4d1931c41
MikaCinc <mihajlo.ls00@outlook.com>
2020-09-03 18:39:28 +0200

    minor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,24 +7,25 @@ const Home = () => {
 
     const login = () => {
         fetch('https://www.googleapis.com/youtube/v3/subscriptions', {
+            cors: 'no-cors',
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 'part': 'id',
+                'id': 'UC18Zl2BCVNzBQx08F-xisVQ',
                 'key': 'AIzaSyCgoPE1Hzq9S6VQFmUf94fU-r_Mf8DTKO8'
             },
-        }).then(response =>
-            console.log(response)
-        )
+        }).then(response => response.json())
+            .then(response => console.log(response));
     }
 
     return (
         <Fragment>
             <h2 className="page-title">Enter this week’s Giveaway for n Minecoins! Ends in: 5d 3h 25s</h2>
             <p>Step 1 (2 points): Subscribe to Minecoin King YouTube channel</p>
-            <p>Step 2 (1 point): Like this video from Minecoing King</p>
-            <p>Step 3 (1 point) Leave a comment on this video from Minecoin King</p>
-            <p>Step 4 (2 points) Share the Giveaway video</p>
+            <p>Step 2 (1 point): Like this video from Minecoin King</p>
+            <p>Step 3 (1 point): Leave a comment on this video from Minecoin King</p>
+            <p>Step 4 (2 points): Share the Giveaway video</p>
 
             <h4>After you completed steps above, click on this button to login with youtube so we can check your results.</h4>
             <h4>Please note: we will also verify manually all the steps you completed if we pick you as winner.</h4>
